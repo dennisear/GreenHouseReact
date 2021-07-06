8ae4fe32416cd61b579f660c28e15ec579f72f35
katerin_Torres <katerintorres949@gmail.com>
2021-07-06 17:01:32 -0500

    part 'who we are', advanced

--- src/components/BodyAU.jsx
+++ src/components/BodyAU.jsx
@@ -1,8 +1,12 @@
 import React from 'react' // import ALWAYS react //
+import '../utils/css/bodyAUStyles.css'
+import Barra from '../utils/images/bannerAS.png'
 
 const BodyAU = () => {
     return (
-        <div>HOLII soy quiénes somos</div>
+        <div className="banner">
+        <img id="Barra" src={Barra} alt="Cargando..."/></div>
+        
     )
 }
 export default BodyAU
--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -1,11 +1,15 @@
 import React from "react"
 import {Header, BodyAU} from "../../components"
+import '../../utils/css/bodyAUStyles.css'
 
 const AboutUs = () =>{
 	return(
 	<>
 	  <Header/>
       <BodyAU/>
+	  <div className="objetivos">
+	  <h3>Green house es un sitio web creado con el fin de poder brindarles el reconocimiento adecuado a los horticultores y destacar la biodiversidad de árboles y plantas que estos emplean.</h3>
+	  </div>
 	</>
 	)
 }
